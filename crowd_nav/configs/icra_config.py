cb6a23d1fa920c1f6b85130a5d9f78565741ea12
Changan Chen <changanvr@gmail.com>
2019-02-25 17:57:31 -0800

    add icra config

--- crowd_nav/configs/icra_config.py
+++ crowd_nav/configs/icra_config.py
@@ -4,7 +4,7 @@ class Config(object):
 
 class EnvConfig(object):
     env = Config()
-    env.time_limit = 30
+    env.time_limit = 25
     env.time_step = 0.25
     env.val_size = 100
     env.test_size = 500
@@ -18,16 +18,16 @@ class EnvConfig(object):
     reward.discomfort_penalty_factor = 0.5
 
     sim = Config()
-    sim.train_val_scenario = 'square_crossing'
-    sim.test_scenario = 'square_crossing'
-    sim.square_width = 20
-    sim.circle_radius = 5
-    sim.human_num = 20
+    sim.train_val_scenario = 'circle_crossing'
+    sim.test_scenario = 'circle_crossing'
+    sim.square_width = 10
+    sim.circle_radius = 4
+    sim.human_num = 5
     sim.centralized_planning = True
 
     humans = Config()
     humans.visible = True
-    humans.policy = 'socialforce'
+    humans.policy = 'orca'
     humans.radius = 0.3
     humans.v_pref = 1
     humans.sensor = 'coordinates'
@@ -71,7 +71,7 @@ class PolicyConfig(object):
     lstm_rl.mlp2_dims = [150, 100, 100, 1]
     lstm_rl.multiagent_training = True
     lstm_rl.with_om = False
-    lstm_rl.with_interaction_module = True
+    lstm_rl.with_interaction_module = False
 
     srl = Config()
     srl.mlp1_dims = [150, 100, 100, 50]
@@ -86,7 +86,7 @@ class PolicyConfig(object):
     sarl.mlp3_dims = [150, 100, 100, 1]
     sarl.multiagent_training = True
     sarl.with_om = False
-    sarl.with_global_state = False
+    sarl.with_global_state = True
 
     def __init__(self, debug=False):
         pass
@@ -97,7 +97,7 @@ class TrainConfig(object):
     trainer.batch_size = 100
 
     imitation_learning = Config()
-    imitation_learning.il_episodes = 2000
+    imitation_learning.il_episodes = 3000
     imitation_learning.il_policy = 'orca'
     imitation_learning.il_epochs = 50
     imitation_learning.il_learning_rate = 0.01
@@ -122,7 +122,7 @@ class TrainConfig(object):
 
     def __init__(self, debug=False):
         if debug:
-            self.imitation_learning.il_episodes = 10
+            self.imitation_learning.il_episodes = 20
             self.imitation_learning.il_epochs = 5
             self.train.train_episodes = 1
             self.train.checkpoint_interval = self.train.train_episodes
