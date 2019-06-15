b244bc7e70d181f779bcbde5285f83cb92db532c
hushah <sha_hu@sfu.ca>
2019-06-15 15:10:49 -0700

    new file:   crowd_nav/configs/model_predictive_cc5.py         test il model on train episodes:   crowd_nav/train.py

---
+++ crowd_nav/configs/model_predictive_cc5.py
@@ -0,0 +1,37 @@
+from crowd_nav.configs.config import BaseEnvConfig, BasePolicyConfig, BaseTrainConfig, Config
+
+
+class EnvConfig(BaseEnvConfig):
+    def __init__(self, debug=False):
+        super(EnvConfig, self).__init__(debug)
+
+        self.sim.train_val_scenario = 'circle_crossing'
+        self.sim.test_scenario = 'circle_crossing'
+        self.sim.square_width = 10
+        self.sim.circle_radius = 4
+        self.sim.human_num = 5
+
+class PolicyConfig(BasePolicyConfig):
+    def __init__(self, debug=False):
+        super(PolicyConfig, self).__init__(debug)
+
+        # gcn
+        self.gcn.num_layer = 2
+        self.gcn.X_dim = 32
+        self.gcn.similarity_function = 'embedded_gaussian'
+        self.gcn.layerwise_graph = False
+        self.gcn.skip_connection = False
+
+        self.action_space.kinematics = 'unicycle'
+        self.action_space.speed_samples = 3
+        self.action_space.rotation_samples = 5
+
+        self.model_predictive_rl = Config()
+        self.model_predictive_rl.planning_depth = 2
+        self.model_predictive_rl.motion_predictor_dims = [64, 5]
+        self.model_predictive_rl.value_network_dims = [32, 100, 100, 1]
+
+
+class TrainConfig(BaseTrainConfig):
+    def __init__(self, debug=False):
+        super(TrainConfig, self).__init__(debug)
